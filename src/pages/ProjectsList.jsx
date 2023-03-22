7de58026af2744ef178611df988696778a4fcfb8
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-22 17:44:42 +0200

    added project sidebar+filter

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -1,15 +1,23 @@
 import { Col } from "react-bootstrap"
+import { useParams } from "react-router-dom"
 import { Project } from "../components/Project"
+import { Sidebar } from "../components/Sidebar"
 import projects from "../data/projects.json"
 import "./ProjectsList.css"
 
 export function ProjectList() {
+    const { tagName } = useParams();
+    let filtered_projects = projects
+    if (tagName !== undefined) {
+        filtered_projects = projects.filter(item => item.technology.find(tag => tag.name === tagName))
+    }
     return (
         <>
+            {/* <Sidebar /> */}
             <h1>Projektai</h1>
-            {projects.map(item => (
-                <Col key={item.id}>
-                    <Project {...item} />
+            {filtered_projects.map(item => (
+                <Col key={item.name}>
+                    <Project key={"project-" + item.name} {...item} />
                 </Col>
             ))}
         </>
