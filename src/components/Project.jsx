942df457d508e85cf9e7b38571b04e1965b97748
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-31 12:05:48 +0300

    added images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
 import eye from "../assets/eye.svg"
 import "./styles/Project.css"
+
 export function Project(props) {
+    const [imageId, setImageId] = useState(0)
+    const [imagesList, setImagesList] = useState([])
+
+
+    function openGallery() {
+        setImageId(0)
+        setImagesList(props.images)
+        if (imagesList.length != 0) {
+            document.getElementById("project-" + props.name).style.display = 'block';
+        }
+    }
+    function closeGallery() {
+        document.getElementById("project-" + props.name).style.display = 'none';
+    }
+
+    const onClickHandlerRight = () => {
+        if (imageId == imagesList.length - 1)
+            setImageId(0);
+        else
+            setImageId(imageId => imageId + 1);
+    }
+
+    const onClickHandlerLeft = () => {
+        if (imageId == 0)
+            setImageId(imagesList.length - 1);
+        else
+            setImageId(imageId => imageId - 1);
+    }
     return (
         <>
+            <div id={"project-" + props.name} className="gallery-slider">
+                <div className="background_blur" onClick={closeGallery} />
+                <img src={props.images[imageId]} className="image" />
+                <div className="icon-box-left" onClick={onClickHandlerLeft} style={{ cursor: 'pointer' }}>
+                    <img src="imgs/back.png" className="icon" />
+                </div>
+                <div className="icon-box-right" onClick={onClickHandlerRight} style={{ cursor: 'pointer' }}>
+                    <img src="imgs/next.png" className="icon" />
+                </div>
+                <div className="bullets">
+                    {
+                        imagesList.map((_item, index) =>
+                        (
+                            <div className="bullet" onClick={() => setImageId(index)} />
+                        ))
+                    }
+                </div>
+            </div>
             <div className="project-box">
                 <div className="project-title bold">
                     <h2>{props.name}</h2>
@@ -26,8 +74,8 @@ export function Project(props) {
                         <span key={"technology-" + item.name} className="badge">{item.name}</span>
                     ))}
                 </div>
-                <div className="project-picture">
-                    <img src={props.image} className="image" />
+                <div className="project-picture" onClick={openGallery}>
+                    <img src={props.images[0]} className="image" />
                 </div>
             </div>
         </>
